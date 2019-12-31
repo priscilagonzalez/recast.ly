493065c71a307f420e93b9034b1bc064d768b4d6
Ryan Powell <ryan.powell.hkrctr@gmail.com>
2019-12-31 12:25:20 -0800

    Video viewer passes shows example video

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@
 ////<VideoList videos={exampleVideoData}/>
 import VideoList from './VideoList.js';
 import exampleVideoData from '../data/exampleVideoData.js';
+import VideoPlayer from './VideoPlayer.js';
 
 var App = () => (
   <div>
@@ -13,7 +14,7 @@ var App = () => (
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <div><h5><em>videoPlayer</em> <VideoPlayer video={exampleVideoData[0]} /></h5></div>
       </div>
       <div className="col-md-5">
         <div><h5><em>videoList</em> <VideoList videos={exampleVideoData}/> </h5></div>
