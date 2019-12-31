59231270c99533b25b899686388cd54472f3df87
Ryan Powell <ryan.powell.hkrctr@gmail.com>
2019-12-31 07:45:26 -0800

    Getting ready to experiment

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -8,9 +8,15 @@ var VideoList = () => (
   </div>
 );
 
+//What do we want to do with exampleVideoData?
+
+//instantiate VideoList component with dummy data
+//<TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe sleep']}/> // Here we are creating an instance of the `TodoList` component
+
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoList.propTypes = {
+//how do  we pass videos props ? Do we do it here?
   videos: React.PropTypes.array.isRequired
 };
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
+import VideoList from './components/VideoList.js';
+
+ReactDOM.render(<App/>, document.getElementById("app"));
+ReactDOM.render(<VideoList/>, document.getElementByClass("col-md-5"));
+
+//What doesnt work here?
+
+// what did we create on line 28 of index.html?
+
+// What happens on line 3? Somehow... the tag recives the App.jsx component
+
+// Why js and not jsx in imports?
+
+//why is app.js vs app.jsx
 
